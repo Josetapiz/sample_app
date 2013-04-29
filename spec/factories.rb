89b9a43381c01a2b8bb8c4ebe16246b290c46ff5
Jose M. Tapizquent <josetapiz@hotmail.com>
2013-04-28 23:22:22 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Jose Tapizquent"
+    email    "josetapiz@hotmail.com"
+    password "120256"
+    password_confirmation "120256"
+  end
+end
