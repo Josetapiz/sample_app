41d22af00da2fff3b72574bef1e6ca2f322ed898
Jose M. Tapizquent <josetapiz@hotmail.com>
2013-05-01 17:56:37 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
+      sign_in @user
       flash[:success] = "Welcome to ChemTa!"
   		redirect_to @user
   	else
