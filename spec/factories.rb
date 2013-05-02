02a55c1c7ada2c29c45e91d76d1318a9ff2f8704
Jose M. Tapizquent <josetapiz@hotmail.com>
2013-05-02 02:35:25 -0400

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Jose Tapizquent"
-    email    "josetapiz@hotmail.com"
-    password "120256"
-    password_confirmation "120256"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
