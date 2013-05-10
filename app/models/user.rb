955555a6b57f8339caf964356b7f1aea884f74e9
Jose M. Tapizquent <josetapiz@hotmail.com>
2013-05-10 00:26:03 -0400

    Made some changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,23 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :photo, :delete_photo
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_attached_file :photo,
+      :styles => {
+      :thumb=> "100x100#",
+      :small  => "65x65>",
+      :url => "/images/:attachment/:id_:style.:extension",
+      :path => ":rails_root/public/images/:attachment/:id_:style.:extension" }
+
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 
   before_save { email.downcase! }
   before_save :create_remember_token
+  before_validation :clear_photo
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -29,6 +40,20 @@ class User < ActiveRecord::Base
     # This is preliminary. See "Following users" for the full implementation.
     Micropost.where("user_id = ?", id)
   end
+
+   def delete_photo=(value)
+    @delete_photo = !value.to_i.zero?
+  end
+  
+  def delete_photo
+    !!@delete_photo
+  end
+  alias_method :delete_photo?, :delete_photo
+
+  def clear_photo
+    self.photo = nil if delete_photo? && !photo.dirty?
+  end
+
 
   private
 
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,10 +1,10 @@
 namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
-    admin = User.create!(name: "Example User",
-                 email: "example@railstutorial.org",
-                 password: "foobar",
-                 password_confirmation: "foobar")
+    admin = User.create!(name: "Jose Tapizquent",
+                 email: "josetapiz@hotmail.com",
+                 password: "120256",
+                 password_confirmation: "120256")
     admin.toggle!(:admin)
     99.times do |n|
       name  = Faker::Name.name
